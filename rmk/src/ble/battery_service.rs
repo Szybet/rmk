9bf2d3326ba80754080918add8906d20da28cae4
Szybet <53944559+Szybet@users.noreply.github.com>
2026-04-04 19:57:49 +0200

    Don't block battery events

--- rmk/src/ble/battery_service.rs
+++ rmk/src/ble/battery_service.rs
@@ -1,15 +1,9 @@
-use core::sync::atomic::Ordering;
-
-use embassy_futures::join::join;
-use embassy_futures::select::{Either, select};
 use embassy_sync::pubsub::Subscriber;
-use embassy_time::{Duration, Instant, Timer, with_timeout};
+use embassy_time::{Duration, Timer, with_timeout};
 use trouble_host::prelude::*;
 
 use super::ble_server::Server;
-use crate::ble::SLEEPING_STATE;
 use crate::event::{BatteryStateEvent, SubscribableEvent};
-use crate::keyboard::LAST_KEY_TIMESTAMP;
 
 /// Battery service
 #[gatt_service(uuid = service::BATTERY)]
@@ -31,6 +25,7 @@ pub(crate) struct BleBatteryServer<'stack, 'server, 'conn, P: PacketPool> {
         { crate::BATTERY_STATE_EVENT_SUB_SIZE },
         { crate::BATTERY_STATE_EVENT_PUB_SIZE },
     >,
+    pub(crate) state: BatteryStateEvent,
 }
 
 impl<'stack, 'server, 'conn, P: PacketPool> BleBatteryServer<'stack, 'server, 'conn, P> {
@@ -39,6 +34,7 @@ impl<'stack, 'server, 'conn, P: PacketPool> BleBatteryServer<'stack, 'server, 'c
             battery_level: server.battery_service.level,
             conn,
             sub: BatteryStateEvent::subscriber(),
+            state: BatteryStateEvent::NotAvailable,
         }
     }
 }
@@ -67,13 +63,17 @@ impl<P: PacketPool> BleBatteryServer<'_, '_, '_, P> {
 
         // Report the battery level.
         loop {
-            let battery_state = self.wait_until_battery_state_available().await;
+            self.wait_until_battery_state_available().await;
 
-            // Try to receive the latest message
-            if let BatteryStateEvent::Normal(level) = self.sub.try_next_message_pure().unwrap_or(battery_state)
-                && let Err(e) = self.battery_level.notify(self.conn, &level).await
-            {
-                error!("Failed to notify battery level: {:?}", e);
+            // Drain latest messages
+            while let Some(s) = self.sub.try_next_message_pure() {
+                self.state = s;
+            }
+
+            if let BatteryStateEvent::Normal(level) = self.state {
+                if let Err(e) = self.battery_level.notify(self.conn, &level).await {
+                    error!("Failed to notify battery level: {:?}", e);
+                }
             }
         }
     }
@@ -84,33 +84,13 @@ impl<P: PacketPool> BleBatteryServer<'_, '_, '_, P> {
     /// 1. There's a battery state update
     /// 2. There's a key press in last 1 minute, or timeout(30 minutes)
     /// 3. The keyboard is not in the sleep mode
-    async fn wait_until_battery_state_available(&mut self) -> BatteryStateEvent {
+    async fn wait_until_battery_state_available(&mut self) {
         loop {
-            // Calculate timeout when reporting battery level
-            let timeout = async {
-                loop {
-                    embassy_time::Timer::after_secs(1800).await;
-                    // 30 minutes passed and the keyboard isn't in sleep mode: timeout
-                    if !SLEEPING_STATE.load(Ordering::Acquire) {
-                        break;
-                    }
-                }
-            };
-
-            // Wait until there are both battery state update and key pressing or timeout
-            let (battery_state, last_press) =
-                join(self.sub.next_message_pure(), select(timeout, LAST_KEY_TIMESTAMP.wait())).await;
-
-            // Then check the value last press time
-            let last_press = match last_press {
-                Either::First(_) => Instant::now().as_secs() as u32,
-                Either::Second(last_press) => last_press,
-            };
-
-            // Only report battery state if the last key action is less than 60 seconds ago
-            let current_time = Instant::now().as_secs() as u32;
-            if current_time.saturating_sub(last_press) < 60 {
-                return battery_state;
+            self.state = self.sub.next_message_pure().await;
+            if self.state == BatteryStateEvent::NotAvailable {
+                continue;
+            } else {
+                return;
             }
         }
     }
